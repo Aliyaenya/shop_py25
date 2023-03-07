af7e77adf23c62134af4ff55a2f3850662c3ae99
Aliyaenya <aliya.kerimbekova@gmail.com>
2023-03-07 13:53:45 +0600

    addet test

---
+++ spam/serializers.py
@@ -0,0 +1,14 @@
+from spam.models import Contact
+from rest_framework import serializers
+
+class ContactSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=False)
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+    def create(self, validated_data):
+        if Contact.objects.filter(email=validated_data['email']).exists():
+            raise serializers.ValidationError('Вы уже подписались')
+        return super().create(validated_data)
